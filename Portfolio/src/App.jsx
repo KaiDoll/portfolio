c752f8ba7710f2413a9c5260bc2a575717a0f075
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-11-30 20:24:05 -0600

    react-router-dom used to create routes

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,8 +1,23 @@
+import { Outlet } from "react-router-dom";
+import Navigation from "./components/Navigation";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import{ Container } from "react-bootstrap";
+import '../src/App.css'
 
-const App = () => <div>
-<Header />
-<Footer />;
-</div>
+function App() {
+  return (
+    <>
+      <Navigation />
+      <main className="container">
+        <Header />
+        <Container>
+        <Outlet />
+        </Container>
+        <Footer />
+      </main>
+    </>
+  );
+}
+
 export default App;
--- Portfolio/src/main.jsx
+++ Portfolio/src/main.jsx
@@ -1,5 +1,38 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import App from "./App";
+import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from "./components/pages/Contact";
+import Resume from "./components/pages/Resume";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: "/Portfolio",
+        element: <Portfolio />,
+      },
+      {
+        path: "/Contact",
+        element: <Contact />,
+      },
+      {
+        path: "/Resume",
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
