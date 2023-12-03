6f5ebf0fee75e1292451be33bccec34df30a24e8
KaiDoll <132765530+KaiDoll@users.noreply.github.com>
2023-12-03 09:55:51 -0600

    card grid flex sorted

--- Portfolio/src/components/Project.jsx
+++ Portfolio/src/components/Project.jsx
@@ -3,18 +3,17 @@ import Card from "react-bootstrap/Card";
 
 function Project(props) {
   return (
-    <div className="d-flex align-items-start">
-    <Card sm={3} style={{ width: "18rem", margin: "5rem" }}>
+    <Card sm={3} style={{ width: "18rem", margin: "1rem" }}>
       <Card.Img variant="top" src={props.image} />
       <Card.Body>
         <Card.Title>Name: {props.name}</Card.Title>
         {/* <Card.Title>ID: {props.id}</Card.Title> */}
         <Card.Text>Description: {props.description}</Card.Text>
         <Button variant="outline-dark">
-          <a href= {props.gitLink}>Github</a></Button>
+          <a href={props.gitLink}>Github</a>
+        </Button>
       </Card.Body>
     </Card>
-    </div>
   );
 }
 
--- Portfolio/src/components/pages/Portfolio.jsx
+++ Portfolio/src/components/pages/Portfolio.jsx
@@ -1,79 +1,74 @@
 import React from "react";
 import Project from "../Project";
 
-
 const projects = [
   {
     id: 1,
     name: "My Work Scheduler",
-    description: "A simple calendar application that allows a user to save events for each hour of a typical working day",
+    description:
+      "A simple calendar application that allows a user to save events for each hour of a typical working day",
     gitLink: "https://kaidoll.github.io/my-work-scheduler/",
-    image: "https://github.com/KaiDoll/my-work-scheduler/raw/main/image-1.png"
+    image: "https://github.com/KaiDoll/my-work-scheduler/raw/main/image-1.png",
   },
   {
     id: 2,
     name: "Shelf-Scan",
-    description: "A team project where we developed a webpage that would allow us to use server API's to display information of our choosing. ",
+    description:
+      "A team project where we developed a webpage that would allow us to use server API's to display information of our choosing. ",
     gitLink: "https://kaidoll.github.io/shelfscan/",
-    image: "https://github.com/KaiDoll/shelfscan/raw/main/assets/images/frontpage.png"
+    image:
+      "https://github.com/KaiDoll/shelfscan/raw/main/assets/images/frontpage.png",
   },
   {
     id: 3,
     name: "SVG-Generator",
-    description: "SVG generated through Node.js command-line application that takes in user input to generate a logo and save it as an SVG fileLinks to an external site.",
+    description:
+      "SVG generated through Node.js command-line application that takes in user input to generate a logo and save it as an SVG fileLinks to an external site.",
     gitLink: "https://github.com/KaiDoll/SVGshapeGeneretor",
-    image: "https://github.com/KaiDoll/SVGshapeGeneretor/raw/main/image-1.png"
+    image: "https://github.com/KaiDoll/SVGshapeGeneretor/raw/main/image-1.png",
   },
   {
     id: 4,
     name: "Employee Tracker",
-    description: "A content management systems (CMS) interfaces that allow non-developers to easily view and interact with information stored in databases.",
+    description:
+      "A content management systems (CMS) interfaces that allow non-developers to easily view and interact with information stored in databases.",
     gitLink: "https://github.com/KaiDoll/EmployeeTracker",
-    image: "https://github.com/KaiDoll/EmployeeTracker/raw/main/image.png"
+    image: "https://github.com/KaiDoll/EmployeeTracker/raw/main/image.png",
   },
   {
     id: 5,
     name: "Stress Free Me",
-    description: "An innovative web application designed to guide users through a series of targeted inquiries, facilitating a comprehensive assessment of individual stress levels",
+    description:
+      "An innovative web application designed to guide users through a series of targeted inquiries, facilitating a comprehensive assessment of individual stress levels",
     gitLink: "https://projecttwofromgroupone-f2f987471d78.herokuapp.com/",
-    image:"https://github.com/bmancuso3/stress-free-me/raw/main/assets/images/login.1.png" 
+    image:
+      "https://github.com/bmancuso3/stress-free-me/raw/main/assets/images/login.1.png",
   },
   {
     id: 6,
     name: "Project 3",
     description: "Project in progress",
     gitLink: "https://github.com/KaiDoll",
-    image: "https://cdn.pixabay.com/photo/2023/11/14/15/46/nikon-8388022_1280.jpg"
+    image:
+      "https://cdn.pixabay.com/photo/2023/11/14/15/46/nikon-8388022_1280.jpg",
   },
 ];
 
 export default function Portfolio() {
   return (
-    <div>
-    {projects.map(projects=><Project key={projects.id} name={projects.name} description={projects.description} id={projects.id} image={projects.image} gitLink= {projects.gitLink}/>)}
-</div>
+    <div className="d-flex justify-content-center flex-wrap">
+      {projects.map((projects) => (
+        <Project
+          key={projects.id}
+          name={projects.name}
+          description={projects.description}
+          id={projects.id}
+          image={projects.image}
+          gitLink={projects.gitLink}
+        />
+      ))}
+    </div>
   );
 }
 
 //put your challenges in here and give id
-{/* <div className="d-flex align-items-start">
-<Container fluid>
-  <Row>
-    <Col sm={4} key={props.id}>
-      <Card>
-        <Card.Img variant="top" src={props.image} />
-        <Card.Body>
-          <Card.Title>Name: {props.name}</Card.Title>
-          {/* <Card.Title>ID: {props.id}</Card.Title> */}
-          <Card.Text>
-            <b>Description: </b> {props.description}
-          </Card.Text>
-          <Button variant="outline-dark">
-            <a href={props.gitLink}>Github</a>
-          </Button>
-        </Card.Body>
-      </Card>
-    </Col>
-  </Row>
-</Container>
-</div> */}
